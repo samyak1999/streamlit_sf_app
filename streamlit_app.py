4b6c9eb775af1fa7a3ecfa6705ecc2a864d7943c
Samyak <43057462+samyak1999@users.noreply.github.com>
2022-11-22 15:01:53 +0530

    Update streamlit_app.py
    Multiselect picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,11 @@ streamlit.text('🐔Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
+# Creating a pick list so they can pick the fruit they want to include
+
+streamlit.multiselect('Pick up some fruits:', list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
