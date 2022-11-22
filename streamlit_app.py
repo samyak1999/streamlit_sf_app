ba0f8f91b93a83026620f0b1e00f18ac2d708c61
Samyak <43057462+samyak1999@users.noreply.github.com>
2022-11-22 14:21:17 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Bakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.header('Bakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
